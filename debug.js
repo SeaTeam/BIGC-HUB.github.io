61e0bf8c2bd283ffe26357f2c10d8b142a0e1925
大海 <iwangyang@qq.com>
2016-11-14 20:23:55 +0800

    待解决 鼠标 click 事件

--- debug.js
+++ debug.js
@@ -3,32 +3,6 @@ var log = function() {
         console.log.apply(console, arguments)
     }
     //定义自己的log很重要 enSure增强进化版 穿衣服
-var ensure = function(def, result, message) {
-    if (JSON.stringify(def) !== JSON.stringify(result)) {
-        log('异常————————————>', message)
-        log('def    = "' + def + '"\nresult = "' + result + '"')
-    }
-}
-var ckXian = function(mc) {
-        var body = document.querySelector('body')
-        var style =
-            `<style id="xm" media="screen">
-            ${mc}{outline: 1px red dashed} </style>`
-        var i = false
-        body.addEventListener('keydown', function(event) {
-            if (event.keyCode === 77 && event.ctrlKey) {
-                if (i) {
-                    var styletog = document.querySelector('#xm')
-                    styletog.remove();
-                    i = false
-                } else {
-                    body.insertAdjacentHTML('afterbegin', style);
-                    i = true
-                }
-            }
-        })
-    }
-ckXian('*')
 // 绑定 导航
 $('#id-top').on('mouseover', function() {
     $('.top').fadeIn(618)
@@ -106,31 +80,75 @@ var so = {
         var data = encodeURI(content)
         var sugurl = `https://www.sogou.com/suggnew/ajajjson?type=web&key=${data}`
         //回调函数
-        window.sogou = { sug: function(json){so.sug = json[1]} }
+        window.sogou = { sug: function(json) {
+            so.sug = json[1]
+            $('.search-li').remove()
+            if(so.sug.length === 0) {
+                so.hide()
+            } else {
+                so.show()
+            }
+        }}
         //动态 JS脚本
         var temp =  `<script src=${sugurl}></script>`
         $("#id-Sug").html(temp)
         //http://www.cnblogs.com/woider/p/5805248.html
     },
-    sug:[],
+    sug: [],
+    now: -1,
+    addClass: function(hover) {
+        $('.search-li').each( function(i, e) {
+            if (i === so.now) {
+                var e = $(e)
+                e.addClass('search-li-hover')
+                if (!hover) { //如果不是悬浮
+                    $('.search-input').val(e.text())
+                }
+            } else {
+                $(e).removeClass('search-li-hover')
+            }
+        })
+    },
+    upDown: function(next) {
+        if (so.sug.length > 1) {
+            var all = so.sug.length
+            var old = (so.now  + all) % all
+            so.now  = (old + next) % all
+            so.addClass()
+        } else if (so.sug.length === 1) {
+            $('.search-li').addClass('search-li-hover')
+        }
+    },
+    hide: function() {
+        $('.search-list').css('border-color','transparent')
+    },
+    show: function() {
+        $('.search-list').css('border-color','#037dd8')
+        for (var i = 0; i < so.sug.length; i++) {
+            $('.search-list').append(`<div data-id=${i} class="search-li">${so.sug[i]}</div>`)
+        }
+        $('.search-list').append(`<div class="search-space"><i class="fa fa-angle-double-up fa-lg" aria-hidden="true"></i></div>`)
+        $('.search-space')[0].remove()
+    },
 }
+$('.search-list' ).on('mouseover', '.search-li', function(event) {
+    so.now = Number(event.target.dataset.id)
+    var hover = true
+    so.addClass(hover)
+})
+$('.search-list' ).on('click', '.search-li', function(event) {
+    log('123')
+})
 $('.search-input').on('keyup', function() {
-    // log(event.keyCode)
     if (event.keyCode === 13) {
         $('.search-button').click()
+    } else if (event.keyCode === 38) {
+        so.upDown(-1)
     } else if (event.keyCode === 40) {
-        log('下')
+        so.upDown(1)
     } else {
-        $('.search-li').remove()
         so.gou(event.target.value)
-        if(so.sug.length === 0) {
-            $('.search-list').css('border-color','transparent')
-        } else {
-            $('.search-list').css('border-color','#037dd8')
-            for (i of so.sug) {
-                $('.search-list').prepend(`<div class="search-li">${i}</div>`)
-            }
-        }
+        so.now = -1
     }
 })
 $('.search-input').on('blur', function() {
@@ -139,12 +157,9 @@ $('.search-input').on('blur', function() {
 })
 $('.search-input').on('focus', function() {
     if(so.sug.length === 0) {
-        $('.search-list').css('border-color','transparent')
+        so.hide()
     } else {
-        $('.search-list').css('border-color','#037dd8')
-        for (i of so.sug) {
-            $('.search-list').prepend(`<div class="search-li">${i}</div>`)
-        }
+        so.show()
     }
 })
 // 2. engine自动排序 和 添加/删除 功能engine
